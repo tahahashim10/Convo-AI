7176b2cd3bd71ef27093d10a8abd3c418c0aa780
Taha Hashim <tahahashim10@gmail.com>
2024-06-25 22:30:57 -0400

    setting up frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/AppBar';
+
+const Header = () => {
+  return ( 
+    <AppBar sx={{}}>
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+};
+export default Header;
